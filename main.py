8ed1bb9390240b2c14ddef98bf4b1863b053a385
perzpio0 <piotr.perz@orange.com>
2024-04-30 14:45:19 +0200

    Day 27: Access the news titles and descriptions

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
-api_key= "81acb547848a4d17b543dfa4144a7daa"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-03-30&sortBy=publishedAt&apiKey=81acb547848a4d17b543dfa4144a7daa"
+api_key = "81acb547848a4d17b543dfa4144a7daa"
+url = f"https://newsapi.org/v2/everything?q=poland&" \
+      f"from=2024-03-30&sortBy=publishedAt&apiKey=" \
+      f"{api_key}"
 
 request = requests.get(url)
-
-content = request.text
-print(content)
+content = request.json( )
+for item in content["articles"]:
+      print(item["title"])
+      print(item["description"])
